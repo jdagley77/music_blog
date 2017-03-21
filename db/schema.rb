e885f5b525136ec1776fa096dda7c644eabe2d3f
Jack Dagley <jack.dagley77@gmail.com>
2017-03-21 12:22:46 -0700

    Revert to simpler schema, changes time display

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320235437) do
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-  end
+ActiveRecord::Schema.define(version: 20170320233550) do
 
   create_table "posts", force: :cascade do |t|
-    t.datetime "post_date"
     t.string   "title"
     t.text     "body"
-    t.string   "genre"
-    t.integer  "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,8 @@
 require 'faker'
 
 Post.destroy_all
-# Category.destroy_all
-
-# 4.times do 
-# 	Category.create!(name: Faker::Lorem.word)
-# end
 
 10.times do 
-	Post.create!(post_date: Faker::Time.between(2.days.ago, Date.today, :all), title: Faker::RockBand.name, body: Faker::Hipster.paragraph, genre: Faker::Lorem.word, category_id: Category.all.sample.id)
+	Post.create!(created_at: Faker::Time.between(2.days.ago, Date.today, :all), title: Faker::RockBand.name, body: Faker::Hipster.paragraph)
 end
 
