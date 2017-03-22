2e51cbb7767d27c3751679396dbec79bb5699bae
Jack Dagley <jack.dagley77@gmail.com>
2017-03-22 10:49:53 -0700

    Add tag sorting config

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,30 @@ class PostsController < ApplicationController
 
 	def new 
 	end
+
+	def create
+  		@post = Post.new(post_params)
+  		respond_to do |format|
+    if @post.save
+      	format.js # Will search for create.js.erb
+    else
+      	format.html { render root_path }
+    end
+	end
+  end
 	
 	def index
-		@posts = Post.all
+		# @posts = Post.all
 		@news = MtvNews.news_feed
+		if params[:tag]
+    		@posts = Post.tagged_with(params[:tag])
+  		else
+    		@posts = Post.all
+  end
 	end
 
+	private
+		def post_params
+  			params.require(:post).permit(:author, :content, :all_tags)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
 		resources :posts 
-		resources :categories
-end
+		get 'tags/:tag', to: 'posts#index', as: "tag"
+	end
