e1f503b6fae306da69eea7588d3356adb524d5c5
Jack Dagley <jack.dagley77@gmail.com>
2017-03-26 09:59:44 -0700

    Change tags to genres and minimize duplicates for each sample post

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,9 @@ require 'faker'
 Post.destroy_all
 Tag.destroy_all
 
-5.times do 
-	Tag.create!(name: Faker::Music.instrument)
-end
+TAGS = ["Blues", "Classical", "Country", "Electronic", "International", "Jazz", "Pop", "Reggae"]
+
+TAGS.each { |tag| Tag.create!(name: tag) }
 
 10.times do 
 	Post.create!(created_at: Faker::Time.between(2.days.ago, Date.today, :all), title: Faker::RockBand.name, body: Faker::Hipster.paragraph)
