af3606539d740dc58ccdd7e98d61321197ef58ed
Jack Dagley <jack.dagley77@gmail.com>
2017-04-12 11:15:30 -0700

    Add readme

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,13 @@ class PostsController < ApplicationController
 
 	def create
 		@post = Post.new(post_params)
-		respond_to do |format|
-	    if @post.save
-	      	format.js # Will search for create.js.erb
-	    else
-	      	format.html { render root_path }
-	    end
-		end
+		# respond_to do |format|
+	 #    if @post.save
+	 #      	format.js # Will search for create.js.erb
+	 #    else
+	 #      	format.html { render root_path }
+	 #    end
+		# end
   	end
 	
 	def index
